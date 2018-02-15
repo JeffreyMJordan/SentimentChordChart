9c113829d133f100637438bc86c9a247ad757eec
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2018-02-15 11:07:15 -0800

    adds chords and ribbons

--- chord_chart.js
+++ chord_chart.js
@@ -12,8 +12,16 @@ window.addEventListener('DOMContentLoaded', () => {
   let outerRadius = Math.min(width, height)*0.5-40;
   let innerRadius = outerRadius-30;
 
-  let arc = d3.arc().innerRadius(innerRadius).outerRadius(outerRadius).startAngle(45 * (Math.PI/180)).endAngle(3);
-  console.log(arc);
-  svg.append("path").attr("d", arc);
+  let arc = d3.arc().innerRadius(innerRadius).outerRadius(outerRadius);
+
+  let chord = d3.chord().padAngle(0.05).sortSubgroups(d3.descending);
+  
+  //What does a ribbon's radius represent? This isn't obvious to me
+  let ribbon = d3.ribbon().radius(innerRadius);
+  let colorScale = d3.scaleOrdinal().domain([1, 3]).range(['#FF0000', '#00FF00', '#00FF00']);
+
+  let g = svg.append("g")
+  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+  .datum(chord(matrix));
   
 });
